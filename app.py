586e2b6b3d90b8ba14ebe6bdce6d06bec4046802
Benson Mugure <b.mugure@alustudent.com>
2023-12-23 18:51:53 +0400

    display the pygwalker dashboard in streamlit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import pygwalker as pyg
+import streamlit.components.v1 as components
+from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+
+# set page configurations
+st.set_page_config(
+    page_title="PygWalker - An intro",
+    page_icon="🐾",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Load data
+@st.cache_data
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+df = load_data("workspace/sleep_health_data.csv")
+
+# Set title and subtitle
+st.title("PygWalker - An intro")
+st.subheader("A simple app to introduce PygWalker")
+
+# Display PygWalker
+def load_config(file_path):
+    with open(file_path, "r") as f:
+        config_str = f.read()
+    return config_str
+
+config = load_config('config.json')
+
+
+# The following took quite a bit of iteration
+pyg_html = pyg.walk(df, spec=config, dark='dark', return_html=True)
+
+components.html(pyg_html, height=1000, scrolling=True)
